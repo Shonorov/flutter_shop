5b53ec28566377e34348adb6774394373a9ebade
shonorov <shonorov@gmail.com>
2019-08-12 16:32:47 +0300

    fetch orders with spinner

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -22,6 +22,33 @@ class Orders with ChangeNotifier {
 
   List<OrderItem> get orders => [..._orders];
 
+  Future<void> fetchAndSetOrders() async {
+    const url = 'https://mshonorov.firebaseio.com/orders.json';
+    final response = await http.get(url);
+    final List<OrderItem> loadedOrders = [];
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+    if (extractedData == null) return;
+    extractedData.forEach((orderId, orderData) {
+      loadedOrders.add(
+        OrderItem(
+          id: orderId,
+          amount: orderData['amount'],
+          products: (orderData['products'] as List<dynamic>).map(
+            (item) => CartItem(
+              id: item['id'],
+              price: item['price'],
+              quantity: item['quantity'],
+              title: item['title'],
+            ),
+          ).toList(),
+          dateTime: DateTime.parse(orderData['dateTime']),
+        ),
+      );
+    });
+    _orders = loadedOrders.reversed.toList();
+    notifyListeners();
+  }
+
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
     const url = 'https://mshonorov.firebaseio.com/orders.json';
     final timeStamp = DateTime.now();
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -4,12 +4,33 @@ import 'package:provider/provider.dart';
 import 'package:flutter_shop/widgets/order_item.dart';
 import 'package:flutter_shop/widgets/app_drawer.dart';
 
-class OrdersScreen extends StatelessWidget {
+class OrdersScreen extends StatefulWidget {
   static const routeName = '/orders';
 
   @override
-  Widget build(BuildContext context) {
+  _OrdersScreenState createState() => _OrdersScreenState();
+}
 
+class _OrdersScreenState extends State<OrdersScreen> {
+  var _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      //Will be executed after widget initialization
+      setState(() {
+        _isLoading = true;
+      });
+      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
 
     return Scaffold(
@@ -17,12 +38,14 @@ class OrdersScreen extends StatelessWidget {
         title: Text('Your orders'),
       ),
       drawer: AppDrawer(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (context, i) => OrderItem(
-          orderData.orders[i],
-        ),
-      ),
+      body: _isLoading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (context, i) => OrderItem(
+                orderData.orders[i],
+              ),
+            ),
     );
   }
 }
