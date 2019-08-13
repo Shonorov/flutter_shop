e900953c6ffcf29867e37cad45d4d8eaed75f08b
shonorov <shonorov@gmail.com>
2019-08-13 09:30:07 +0300

    sign in request

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,17 +7,21 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
+  Future<void> _authenticate(String email, String password, String urlSegment) async {
     final url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDTxIGcihv0F4vdN77g6IzDyNvuoBQ1VDk";
-    try {
-      final response = await http.post(url, body: {
-        'email': email,
-        'password': password,
-        'returnSecureToken': true,
-      });
-    } on Exception catch (e) {
-      // TODO
-    }
+        "https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDTxIGcihv0F4vdN77g6IzDyNvuoBQ1VDk";
+    final response = await http.post(url, body: {
+      'email': email,
+      'password': password,
+      'returnSecureToken': true,
+    });
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
