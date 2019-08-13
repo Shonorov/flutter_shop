4001afff357a43a8d168a0b70f5e330658b19ee4
shonorov <shonorov@gmail.com>
2019-08-13 12:20:47 +0300

    manage and fetch only own orders

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -20,13 +20,14 @@ class OrderItem {
 class Orders with ChangeNotifier {
   List<OrderItem> _orders = [];
   final String authToken;
+  final String userId;
 
-  Orders(this.authToken, this._orders);
+  Orders(this.authToken, this.userId, this._orders);
 
   List<OrderItem> get orders => [..._orders];
 
   Future<void> fetchAndSetOrders() async {
-    final url = 'https://mshonorov.firebaseio.com/orders.json?auth=$authToken';
+    final url = 'https://mshonorov.firebaseio.com/orders/$userId.json?auth=$authToken';
     final response = await http.get(url);
     final List<OrderItem> loadedOrders = [];
     final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -53,7 +54,7 @@ class Orders with ChangeNotifier {
   }
 
   Future<void> addOrder(List<CartItem> cartProducts, double total) async {
-    final url = 'https://mshonorov.firebaseio.com/orders.json?auth=$authToken';
+    final url = 'https://mshonorov.firebaseio.com/orders/$userId.json?auth=$authToken';
     final timeStamp = DateTime.now();
     final response = await http.post(
       url,
